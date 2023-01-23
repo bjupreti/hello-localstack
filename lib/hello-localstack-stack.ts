eb000fd13deb4559cd5c024841e4dbf7f1a0647c
Bijay Upreti <bjupreti123@gmail.com>
2023-01-22 21:23:54 -0500

    integrating aws code pipeline

--- lib/hello-localstack-stack.ts
+++ lib/hello-localstack-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class HelloLocalstackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -14,9 +15,24 @@ export class HelloLocalstackStack extends cdk.Stack {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
 
+    const bucketName = new cdk.CfnParameter(this, 'bucketName', {
+      type: 'String',
+      default: '',
+      description: 'S3 bucket name where Codepipeline will store lambda code'
+    });
+
+    const bucketKey = new cdk.CfnParameter(this, 'bucketKey', {
+      type: 'String',
+      default: '',
+      description: 'S3 bucket key which Codepipeline will use to store lambda code'
+    });
+
+    const bucket = s3.Bucket.fromBucketName(this, 'pipeline-bucket', bucketName.valueAsString);
+
     new lambda.Function(this, 'lambdaFunction', {
       functionName: 'first-cdk-lambda',
-      code: new lambda.AssetCode('src'),
+      // code: new lambda.AssetCode('src'),
+      code: lambda.Code.fromBucket(bucket, bucketKey.valueAsString),
       handler: 'index.handler',
       runtime: lambda.Runtime.NODEJS_16_X,
       memorySize: 128
